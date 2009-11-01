253193576a0950d3e6f698bc2d8aafcf1e873bbb
Josh Matthews <josh@redriverit.net>
2009-10-31 19:33:46 -0500

    Added grader.rb

---
+++ grader.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+# grader.rb - a quick app to generate score cards
+
+base_score = 50
+total_questions = 15
+
+def generate_iterator
+	iterator = base_score / total_questions
+	puts iterator
+end
