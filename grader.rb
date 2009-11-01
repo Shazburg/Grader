576fe83868be49d8a25181f4d27bf3c7bb6ad87f
Josh Matthews <josh@redriverit.net>
2009-10-31 19:55:50 -0500

    Prototype working

--- grader.rb
+++ grader.rb
@@ -2,9 +2,15 @@
 # grader.rb - a quick app to generate score cards
 
 base_score = 50
-total_questions = 15
+question = 0
 
-def generate_iterator
-	iterator = base_score / total_questions
-	puts iterator
+puts "What is the total number of questions? "
+total_questions = gets.to_i
+
+iterator = base_score.to_f / total_questions.to_f
+
+while question <= total_questions do
+	current_score = base_score + (question * iterator)
+	puts question.to_s + "\t" + current_score.round.to_s
+	question += 1
 end
